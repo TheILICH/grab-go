a4d8d275ca809dcb8e6b261fdf9383cf53ef351d
Iliyas Bekkhozha <iliyasbek2003@gmail.com>
2024-10-22 04:05:38 +0500

    https

--- start.sh
+++ start.sh
@@ -1,15 +1,12 @@
 sudo docker compose down -v 
 
 sudo docker rmi -f theilich/pm:latest
-sudo docker rmi -f theilich/go_db:latest
 
 sudo docker build -t theilich/pm:latest -f app .
-sudo docker build -t theilich/go_db:latest -f db .
 
 sudo docker login
 
 sudo docker push theilich/pm:latest
-sudo docker push theilich/go_db:latest
 
 sudo docker compose up
 
