9b61312f630c9054aa8ae83f1c9703386562edff
Iliyas Bekkhozha <iliyasbek2003@gmail.com>
2024-10-21 23:43:39 +0500

    starting script

--- start.sh
+++ start.sh
@@ -1,8 +1,16 @@
-docker rmi -f theilich/pm:latest
-docker rmi -f theilich/go_db:latest
+sudo docker rmi -f theilich/pm:latest
+sudo docker rmi -f theilich/go_db:latest
 
-docker build -t theilich/pm:latest -f app .
-docker build -t theilich/go_db:latest -f db .
+sudo docker build -t theilich/pm:latest -f app .
+sudo docker build -t theilich/go_db:latest -f db .
+
+sudo docker login
+
+sudo docker push theilich/pm:latest
+sudo docker push theilich/go_db:latest
+
+sudo docker compose down -v 
+sudo docker compose up
 
 
 
